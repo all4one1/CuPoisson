b0ffeb84c197840bb0934b3983aabcbf854682cf
all4one1 <asaznai@gmail.com>
2024-11-16 01:26:32 +0500

    minus reduce5 in h file

--- CuPoisson.h
+++ CuPoisson.h
@@ -12,60 +12,6 @@
 
 namespace cg = cooperative_groups;
 
-template <class T, unsigned int blockSize>
-__global__ void reduce5(T* g_idata, T* g_odata, unsigned int n) {
-	// Handle to thread block group
-	cg::thread_block cta = cg::this_thread_block();
-	extern __shared__ double sdata[];
-
-	// perform first level of reduction,
-	// reading from global memory, writing to shared memory
-	unsigned int tid = threadIdx.x;
-	unsigned int i = blockIdx.x * (blockSize * 2) + threadIdx.x;
-
-	T mySum = (i < n) ? g_idata[i]  : 0;
-	if (i + blockSize < n) mySum += g_idata[i + blockSize];
-
-	sdata[tid] = mySum;
-	cg::sync(cta);
-
-	// do reduction in shared mem
-	if ((blockSize >= 512) && (tid < 256)) {
-		sdata[tid] = mySum = mySum + sdata[tid + 256];
-	}
-
-	cg::sync(cta);
-
-	if ((blockSize >= 256) && (tid < 128)) {
-		sdata[tid] = mySum = mySum + sdata[tid + 128];
-	}
-
-	cg::sync(cta);
-
-	if ((blockSize >= 128) && (tid < 64)) {
-		sdata[tid] = mySum = mySum + sdata[tid + 64];
-	}
-
-	cg::sync(cta);
-
-	cg::thread_block_tile<32> tile32 = cg::tiled_partition<32>(cta);
-
-	if (cta.thread_rank() < 32) {
-		// Fetch final intermediate sum from 2nd warp
-		if (blockSize >= 64) mySum += sdata[tid + 32];
-		// Reduce final warp using shuffle
-		for (int offset = tile32.size() / 2; offset > 0; offset /= 2) {
-			mySum += tile32.shfl_down(mySum, offset);
-		}
-	}
-
-	// write result for this block to global mem
-	if (cta.thread_rank() == 0)
-	{
-		g_odata[blockIdx.x] = mySum;
-	}
-}
-
 struct CuPoisson
 {
 	unsigned int N = 0, k = 0;
