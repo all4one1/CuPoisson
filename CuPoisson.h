074d7840233cf84f40800714ad82eb9eae6ea293
all4one1 <asaznai@gmail.com>
2024-11-18 00:53:38 +0500

    rework

--- CuPoisson.h
+++ CuPoisson.h
@@ -2,22 +2,25 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 #include <cuda.h>
-//#include <cooperative_groups.h>
-#include <cooperative_groups/reduce.h>
+
 #include <stdio.h>
 #include <iostream>
 #include <iomanip>
-#include <vector>
+#include <fstream>
+#include <utility>
+
 #include "CudaReduction/CuReduction.h"
 
-namespace cg = cooperative_groups;
+
+
+
 
 struct CuPoisson
 {
 	unsigned int N = 0, k = 0;
 	double eps = 0, res = 0, res0 = 0;
-	double eps_iter = 1e-5;
-	CudaReduction *CR = nullptr;
+	double eps_iter = 1e-4;
+	CudaReduction* CR = nullptr;
 	dim3 gridDim, blockDim;
 	size_t smem = 0, Nbytes = 0;
 	cudaStream_t stream = 0;
@@ -25,90 +28,79 @@ struct CuPoisson
 	void** args = nullptr;
 	double* f_dev = nullptr, * f0_dev = nullptr;
 
-	/**
-	 * grid (n of blocks)
-	 * block (n of threads per block)
-	 * N, size of arrays
-	 * kernel
-	 * arguments, first two fields are necessary: {&ptr1, &ptr2, ... }
-	 */
-	CuPoisson(dim3 grid_, dim3 block_, unsigned int N_, void* kernel_, std::vector<void*> args_)
-	{
-		if (args_.size() < 2)
-		{
-			std::cout << "not enough args..." << std::endl;
-			return;
-		}
+	bool logs_out = false;
+	std::ofstream k_write;
 
-		gridDim = grid_;
-		blockDim = block_;
+	/**
+	* main field (variable) to calculate and swap
+	* N - the size of
+	*/
+	void set_main_field(double* f_, double* f0_, unsigned int N_)
+	{
+		f_dev = f_;
+		f0_dev = f0_;
 		N = N_;
 		Nbytes = sizeof(double) * N;
-		kernel = kernel_;
-
-		f_dev = (double*)args_[0];
-		f0_dev = (double*)args_[1];
-
-		set_args(args_);
-
 		CR = new CudaReduction(f_dev, N, 512);
+	}
+
+	/**
+	* kernel - __global__ void kernel function
+	* args - all the arguments of the kernel
+	* grid (n of blocks)
+	* block (n of threads per block)
+	* shared memory and stream are null by default
+	*/
+	void set_kernel(void* kernel_, void** args_, dim3 grid_, dim3 block_, size_t smem_ = 0, cudaStream_t stream_ = 0)
+	{
+		gridDim = grid_;
+		blockDim = block_;
+		kernel = kernel_;
+		args = args_;
+		smem = smem_;
+		stream = stream_;
+	}
+
+	void switch_writting(std::string name_)
+	{
+		logs_out = true;
+		k_write.open(name_ + ".dat");
 	}
 
 	void solve()
 	{
-
 		k = 0;
 		eps = 1.0;
 		res = 0.0;
 		res0 = 0.0;
 
-		for (k = 0; k < 1000000; k++)
+
+		for (k = 1; k < 1000000; k++)
 		{
 			cudaLaunchKernel(kernel, gridDim, blockDim, args, smem, stream);
-
-			res = CR->reduce(true);
+			res = CR->reduce(f_dev, true);
 			eps = abs(res - res0) / res0;
 			res0 = res;
 
-			swap_ptr(&f_dev, &f0_dev);
 
-			if (eps < eps_iter)
-			{
-				break;
-			}
+			std::swap(args[0], args[1]);
+			std::swap(f_dev, f0_dev);
 
+			if (eps < eps_iter)	break;
 			if (k % 1000 == 0) std::cout << "device k = " << k << ", eps = " << eps << std::endl;
-
 		}
-		std::cout << "device k = " << k << ", eps = " << eps << std::endl;
+		if (k > 100) std::cout << "device k = " << k << ", eps = " << eps << std::endl;
+		if (logs_out) k_write << k << " " << res << " " << eps << std::endl;
 	}
-
-	void run_kernel()
-	{
-		cudaLaunchKernel(kernel, gridDim, blockDim, args, smem, stream);
-	}
-
-	private:
-	void set_args(std::vector<void*> args_)
-	{
-		size_t n = args_.size();
-		args = new void* [n];
-
-		for (int i = 0; i < n; i++)
-		{
-			args[i] = args_[i];
-		}
-	}
-	void swap_ptr (double** ptr1, double** ptr2)
-	{	
-		double* temp;	
-		temp = (*ptr1);	
-		(*ptr1) = (*ptr2);	
-		(*ptr2) = temp;
-	};
-
-
-
 };
 
+//swap_one_3<<<gridDim, blockDim>>>(f0_dev, f_dev, N);
 
+//__global__ void swap_one_3(double* f_old, double* f_new, unsigned int N)
+//{
+//	unsigned int i = threadIdx.x + blockIdx.x * blockDim.x;
+//	unsigned int j = threadIdx.y + blockIdx.y * blockDim.y;
+//	unsigned int k = threadIdx.z + blockIdx.z * blockDim.z;
+//	unsigned int l = i + dev.offset * j + dev.offset2 * k;
+//	if (l < N)	f_old[l] = f_new[l];
+//}
